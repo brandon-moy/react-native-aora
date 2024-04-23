9cd5d41725243e27c1271c6454044ebc02ac65f7
Brandon Moy <brandonkaghenmoy@gmail.com>
2024-04-23 11:44:16 -0700

    Set up SafeAreaView and images onto splash screen

--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,26 @@
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import { Image, ScrollView, Text, View } from "react-native";
 import { Link } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../constants";
 
 export default function App() {
   return (
-    <View className="items-center justify-center flex-1 bg-white">
-      <Text className="text-3xl font-pblack">Aora!</Text>
-      <StatusBar style="auto" />
-      <Link href="/home" style={{ color: "blue" }}>
-        Go to Home
-      </Link>
-    </View>
+    <SafeAreaView className="h-full bg-primary">
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
+        <View className="items-center justify-center w-full h-full px-4">
+          <Image
+            source={images.logo}
+            className="w-[130px] h-[84px]"
+            resizeMode="contain"
+          />
+          <Image
+            source={images.cards}
+            className="max-w-[380px] w-full h-[300px]"
+            resizeMode="contain"
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
