cd8ef7820cf8162efbde0bccb7283a6e579b6b84
Brandon Moy <brandonkaghenmoy@gmail.com>
2024-04-24 11:38:14 -0700

    Began layout in auth and fixed imports for Continue with Email button to redirect to signin page

--- app/index.jsx
+++ app/index.jsx
@@ -1,6 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import { Image, ScrollView, Text, View } from "react-native";
-import { Link } from "expo-router";
+import { Redirect, router } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { images } from "../constants";
 import CustomButton from "../components/CustomButton";
@@ -9,7 +9,7 @@ export default function App() {
   return (
     <SafeAreaView className="h-full bg-primary">
       <ScrollView contentContainerStyle={{ height: "100%" }}>
-        <View className="items-center justify-center w-full h-full px-4">
+        <View className="items-center justify-center w-full h-[85vh] px-4">
           <Image
             source={images.logo}
             className="w-[130px] h-[84px]"
