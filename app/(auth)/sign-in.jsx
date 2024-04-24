9745b44654b50ce296314c9a0831b2149e5d77f7
Brandon Moy <brandonkaghenmoy@gmail.com>
2024-04-24 11:49:09 -0700

    Imported and used FormField in sign-in, added props to use in formfield when built

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, ScrollView, Image } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { images } from "../../constants";
+import FormField from "../../components/FormField";
 
 const SignIn = () => {
+  const [form, setForm] = useState({ email: "", password: "" });
+
   return (
     <SafeAreaView className="h-full bg-primary">
       <ScrollView>
@@ -16,6 +19,19 @@ const SignIn = () => {
           <Text className="mt-10 text-2xl text-white text-semibold font-psemibold">
             Log in to Aora
           </Text>
+          <FormField
+            title="Email"
+            value={form.email}
+            handleChangeText={(e) => setForm({ ...form, email: e })}
+            otherStyles="mt-7"
+            keyboardType="email-address"
+          />
+          <FormField
+            title="Password"
+            value={form.password}
+            handleChangeText={(e) => setForm({ ...form, password: e })}
+            otherStyles="mt-7"
+          />
         </View>
       </ScrollView>
     </SafeAreaView>
