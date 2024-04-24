f59db3ee430926b30df76b77c967fa4cf7acdaeb
Brandon Moy <brandonkaghenmoy@gmail.com>
2024-04-24 11:45:41 -0700

    Set up layout for SignIn page with image and text for log in

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,11 +1,24 @@
-import { View, Text } from "react-native";
 import React from "react";
+import { View, Text, ScrollView, Image } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../../constants";
 
 const SignIn = () => {
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+    <SafeAreaView className="h-full bg-primary">
+      <ScrollView>
+        <View className="justify-center w-full h-full px-4 my-6">
+          <Image
+            source={images.logo}
+            resizeMode="contain"
+            className="w-[115px] h-[35px]"
+          />
+          <Text className="mt-10 text-2xl text-white text-semibold font-psemibold">
+            Log in to Aora
+          </Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
