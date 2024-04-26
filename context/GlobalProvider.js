33400b1f7e77ac148a1d06fbe8a634fb8633cdcb
Brandon Moy <brandonkaghenmoy@gmail.com>
2024-04-26 11:04:38 -0700

    Filled in useEffect to getCurrentUser and update global user, isloggedin, and isloading states

--- context/GlobalProvider.js
+++ context/GlobalProvider.js
@@ -1,4 +1,5 @@
 import { createContext, useContext, useState, useEffect } from "react";
+import { getCurrentUser } from "../lib/appwrite";
 
 const GlobalContext = createContext();
 
@@ -9,7 +10,36 @@ const GlobalProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    getCurrentUser()
+      .then((res) => {
+        if (res) {
+          setIsLoggedIn(true);
+          setUser(res);
+        } else {
+          setIsLoggedIn(false);
+          setUser(null);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, []);
 
-  return <GlobalContext.Provider value={{}}>{children}</GlobalContext.Provider>;
+  return (
+    <GlobalContext.Provider
+      value={{
+        isLoggedIn,
+        setIsLoggedIn,
+        user,
+        setUser,
+        isLoading,
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
 };
