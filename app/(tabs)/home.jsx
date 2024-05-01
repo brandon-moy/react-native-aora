7eb0598780f6c290b65ff8660658cb0ae14c2187
Brandon Moy <brandonkaghenmoy@gmail.com>
2024-05-01 13:45:19 -0700

    Added styling to ListHeaderComponent and SafeAreaView to match app style

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,19 +1,33 @@
-import { View, Text, FlatList } from "react-native";
+import { View, Text, FlatList, Image } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../../constants";
 
 const Home = () => {
   return (
-    <SafeAreaView>
+    <SafeAreaView className="bg-primary">
       <FlatList
         data={[{ id: 1 }, { id: 2 }, { id: 3 }]}
         keyExtractor={(item) => item.id}
-        renderItem={({ item }) => <Text className="text-3xl">{item.id}</Text>}
+        renderItem={({ item }) => (
+          <Text className="text-3xl text-white">{item.id}</Text>
+        )}
         ListHeaderComponent={() => (
           <View className="px-4 my-6 space-y-6">
             <View className="flex-row items-start justify-between mb-6">
               <View>
-                <Text>Welcome Back</Text>
-                <Text>brandon</Text>
+                <Text className="text-sm text-gray-100 font-pmedium">
+                  Welcome Back
+                </Text>
+                <Text className="text-2xl text-white font-psemibold">
+                  brandon
+                </Text>
+              </View>
+              <View className="mt-1.5">
+                <Image
+                  source={images.logoSmall}
+                  className="h-10 w-9"
+                  resizeMode="contain"
+                />
               </View>
             </View>
           </View>
