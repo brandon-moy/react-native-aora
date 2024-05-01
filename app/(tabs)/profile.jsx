8032657ceda7e83c237c02be05067da954d9532a
Brandon Moy <brandonkaghenmoy@gmail.com>
2024-05-01 10:43:22 -0700

    Added TouchableOpacity with Image of logout icon to Profile screen

--- app/(tabs)/profile.jsx
+++ app/(tabs)/profile.jsx
@@ -1,9 +1,19 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity, Image } from "react-native";
+import { icons } from "../../constants";
 
 const Profile = () => {
   return (
     <View>
       <Text>Profile</Text>
+      <View className="flex items-center justify-center w-full h-full">
+        <TouchableOpacity>
+          <Image
+            source={icons.logout}
+            resizeMode="contain"
+            className="w-6 h-6"
+          />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
