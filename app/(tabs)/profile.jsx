7c74891b9b6c32f48a40a9845258b2794fb8418d
Brandon Moy <brandonkaghenmoy@gmail.com>
2024-05-01 10:46:18 -0700

    Created logout function that calls signOut to delete session, sets user to null, logged int o false, and redirects user to sign-in screen

--- app/(tabs)/profile.jsx
+++ app/(tabs)/profile.jsx
@@ -1,12 +1,24 @@
 import { View, Text, TouchableOpacity, Image } from "react-native";
 import { icons } from "../../constants";
+import { signOut } from "../../lib/appwrite";
+import { useGlobalContext } from "../../context/GlobalProvider";
+import { router } from "expo-router";
 
 const Profile = () => {
+  const { setUser, setIsLoggedIn } = useGlobalContext();
+  const logout = async () => {
+    await signOut();
+    setUser(null);
+    setIsLoggedIn(false);
+
+    router.replace("/sign-in");
+  };
+
   return (
     <View>
       <Text>Profile</Text>
       <View className="flex items-center justify-center w-full h-full">
-        <TouchableOpacity>
+        <TouchableOpacity onPress={logout}>
           <Image
             source={icons.logout}
             resizeMode="contain"
